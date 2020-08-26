ebf420c2df93592e149ca8eccd62104d75785d11
Prakash Shahi <prakashthakuri1994@gmail.com>
2020-08-26 14:15:34 -0400

    PacMan and the maze

---
+++ script.js
@@ -0,0 +1,99 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid= document.querySelector('.grid')
+    const scoreDisplay = document.getElementById('score')
+    const width = 28 //28 * 28 = 784
+
+    const layout = [
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+        1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+        1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+        1,3,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,3,1,
+        1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+        1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+        1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+        1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+        1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+        1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,
+        1,1,1,1,1,1,0,1,1,4,4,4,4,4,4,4,4,4,4,1,1,0,1,1,1,1,1,1,
+        1,1,1,1,1,1,0,1,1,4,1,1,1,2,2,1,1,1,4,1,1,0,1,1,1,1,1,1,
+        1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+        4,4,4,4,4,4,0,0,0,4,1,2,2,2,2,2,2,1,4,0,0,0,4,4,4,4,4,4,
+        1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+        1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+        1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+        1,0,0,0,0,0,0,0,0,4,4,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,1,
+        1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+        1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+        1,3,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,3,1,
+        1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+        1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+        1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1,
+        1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+        1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+        1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+      ]
+    //Legend
+    //0 - pac-dot
+    //1-wall
+    //2- ghost-liar
+    //3 power-pellet
+    //4 empty
+
+    //draw the grid and render it
+
+    const squares = []
+
+    function createBoard() {
+        for(let i =0; i< layout.length; i++){
+            const square = document.createElement('div')
+            grid.appendChild(square)
+            squares.push(square)
+
+
+            //add layout to the board
+            if(layout[i] === 0) {
+                squares[i].classList.add('pac-dot')
+            }else if(layout[i] === 1){
+                squares[i].classList.add('wall')
+            }else if(layout[i] === 2){
+                //squares[i].classList.add('power-pellet')
+            }else if(layout[i] === 3){
+                squares[i].classList.add('power-pellet')
+            }
+        }
+    }
+    createBoard()
+
+    let pacmanCurrentIndex = 490
+
+    squares[pacmanCurrentIndex].classList.add('pac-man')
+
+    function movePacman(e){
+
+
+    squares[pacmanCurrentIndex].classList.remove('pac-man')
+
+    switch(e.keyCode){
+        case 37:
+            if(pacmanCurrentIndex % width !==0) pacmanCurrentIndex -=1
+            break
+        case 38:
+            if(pacmanCurrentIndex -width >=0) pacmanCurrentIndex -=width
+            break
+        case 39:
+            if(pacmanCurrentIndex % width < width -1) pacmanCurrentIndex +=1
+            break
+        case 40:
+            if(pacmanCurrentIndex + width < width * width) pacmanCurrentIndex +=width
+            break
+        }
+
+        squares[pacmanCurrentIndex].classList.add('pac-man')
+    }
+
+
+
+
+
+})
